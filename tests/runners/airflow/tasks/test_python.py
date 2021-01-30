557fb89f2d9af0e4bc06c552b86fa4e73f187469
lidor ettinger <lidor.ettinger@gmail.com>
2021-01-31 00:46:24 +0200

    redefine tests

--- tests/runners/airflow/tasks/test_python.py
+++ tests/runners/airflow/tasks/test_python.py
@@ -1,16 +1,20 @@
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
 #
-#    http://www.apache.org/licenses/LICENSE-2.0
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-import logging
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
 import os
 import tempfile
 import unittest
@@ -36,7 +40,7 @@ class TestPythonTask(TestCase):
 
         task0 = self.__create_python_task(dag,
                                           'my_input_task',
-                                          [],
+                                          None,
                                           'my_python_task_img',
                                           'python -u write_inputs.py',
                                           env_vars={
@@ -47,14 +51,14 @@ class TestPythonTask(TestCase):
 
         task1 = self.__create_python_task(dag,
                                           'my_output_task',
-                                          [dag.tasks[0]],
+                                          dag.tasks[0],
                                           'my_parallelized_python_task_img',
                                           'python -u write_outputs.py',
                                           executors=3)
         task1.apply_task_to_dag()
 
         for task in dag.tasks:
-            logging.info(f'Executing task {task.task_id}')
+            print(f'Executing task {task.task_id}')
             task.execute({})
 
         inputs_dir = os.path.join(self.temp_dir, 'inputs')
